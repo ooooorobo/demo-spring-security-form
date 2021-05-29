0697cd1a173673ff4bcc8356820559718a1f16b8
ooooorobo <orobos654@gmail.com>
2021-05-30 01:02:04 +0900

    test: Add test for authorization in index, admin pages

--- src/test/java/com/robo/demospringsecurityform/account/AccountControllerTest.java
+++ src/test/java/com/robo/demospringsecurityform/account/AccountControllerTest.java
@@ -1,4 +1,66 @@
+package com.robo.demospringsecurityform.account;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithAnonymousUser;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.anonymous;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 class AccountControllerTest {
-  
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    @WithAnonymousUser
+    public void index_anonymous() throws Exception {
+        mockMvc.perform(get("/").with(anonymous()))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @WithMockUser(username = "robo",roles = "USER")
+    public void index_user() throws Exception {
+        mockMvc.perform(get("/").with(user("robo").roles("USER")))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+    @Test
+    @WithMockUser(username="admin",roles="ADMIN")
+    public void index_admin() throws Exception {
+        mockMvc.perform(get("/").with(user("robo").roles("ADMIN")))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @WithRobo
+    public void admin_user() throws Exception {
+        mockMvc.perform(get("/admin").with(user("robo").roles("USER")))
+                .andDo(print())
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    @WithAdmin
+    public void admin_admin() throws Exception {
+        mockMvc.perform(get("/admin").with(user("robo").roles("ADMIN")))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
 }
---
+++ src/test/java/com/robo/demospringsecurityform/account/WithRobo.java
@@ -0,0 +1,11 @@
+package com.robo.demospringsecurityform.account;
+
+import org.springframework.security.test.context.support.WithMockUser;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+@WithMockUser(username = "robo", roles = "USER")
+public @interface WithRobo {
+}
