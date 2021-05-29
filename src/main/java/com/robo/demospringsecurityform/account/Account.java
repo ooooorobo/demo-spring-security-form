df33dcc9055353a7cf75db45362856b26d12f1bc
ooooorobo <orobos654@gmail.com>
2021-05-29 18:40:18 +0900

    Add JPA and h2DB dependencies

---
+++ src/main/java/com/robo/demospringsecurityform/account/Account.java
@@ -0,0 +1,4 @@
+package com.robo.demospringsecurityform.account;
+
+public class Account {
+}
--- src/main/java/com/robo/demospringsecurityform/config/SecurityConfig.java
+++ src/main/java/com/robo/demospringsecurityform/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.robo.demospringsecurityform.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -22,5 +23,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
         http.httpBasic(); // HTTP Basic 을 사용하겠다
 
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.inMemoryAuthentication()
+//                // {noop} - 해당하는 인코딩 방식으로 입력받은 값을 암호화하여 123 과 일치하면 인증 처리
+//                // 123 -> DB에 입력될 때 암호화되어 저장되어 있음
+//                // 암호 입력 -> {noop} 방식으로 인코딩 (암호화 안했다는 뜻임) -> 암호화된 123과 비교 -> 인증 처리
+//                .withUser("robo").password("{noop}123").roles("USER").and()
+//                .withUser("admin").password("{noop}!@#").roles("ADMIN");
     }
 }
