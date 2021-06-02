8e17068eb3746ea10e5eff821e3a5460941fc99a
ooooorobo <orobos654@gmail.com>
2021-06-02 16:22:54 +0900

    Add SampleService for test Authentication in SecurityContextHolder

---
+++ src/main/java/com/robo/demospringsecurityform/form/SampleService.java
@@ -0,0 +1,24 @@
+package com.robo.demospringsecurityform.form;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+
+@Service
+public class SampleService {
+
+    public void dashboard() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        // 사용자 정보
+        Object principal = authentication.getPrincipal();
+        // 사용자 권한
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        // 인증 여부
+        boolean authenticated = authentication.isAuthenticated();
+
+        
+    }
+}
